fabff7e936e48feff8d557638bb09aa2c53bb305
perjo927 <djpjgj@gmail.com>
2020-09-01 17:53:29 +0200

    1/9

--- src/main.js
+++ src/main.js
@@ -1,92 +1,31 @@
 // import a CSS module
 import classes from "./main.css";
-
-const CONSTS = {
-  actions: {
-    ADD: "ADD",
-    DELETE: "DELETE",
-    UNDO: "UNDO",
-    REDO: "REDO",
-    COMPLETE: "COMPLETE",
-    UNCOMPLETE: "UNCOMPLETE",
-  },
-};
+import { v4 as uuidv4 } from "uuid";
+import { createStore } from "./store";
+import { actions } from "./actions";
+import { undoableReducer } from "./reducer";
 
 // Creates an object which properties are not writable
 // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
-const createTodoItem = (text) =>
-  Object.create(
-    {},
-    {
-      // id: generateID
-      text: { value: text },
-      done: { value: false },
-    }
-  );
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case CONSTS.actions.ADD:
-      return [...state, action.value];
-    case CONSTS.actions.DELETE:
-      return state;
-    case CONSTS.actions.UNDO:
-      // TODO: Find ID
-      return state;
-    case CONSTS.actions.REDO:
-      return state;
-    case CONSTS.actions.COMPLE:
-      return state;
-    case CONSTS.actions.UNCOMPLETE:
-      return state;
-    default:
-      return state;
-  }
-};
-
-const actions = {
-  addTodo: (todo) => {
-    return {
-      type: CONSTS.actions.ADD,
-      value: todo,
-    };
-  },
-};
-
-const createStore = (initialState = {}, reducer) => {
-  // retained by closure
-  const stateContainer = [initialState];
-
-  const getState = () => {
-    const [lastState] = stateContainer.slice(-1); // no mutation
-    return lastState;
-  };
-
-  const setState = (newState) => {
-    stateContainer.push(newState);
-  };
-
-  const dispatch = (action) => {
-    const newState = reducer(getState(), action);
-    setState(newState); // TODO: Impure (side effect), emit event instead and assert that it was called
-    return action;
-  };
-
-  // Object.create
-  return {
-    getState,
-    dispatch,
-  };
+const createTodoItem = (text) => {
+  return { text, done: false, id: uuidv4() };
 };
 
 const newTodoItem = createTodoItem("text");
+const { id } = newTodoItem;
 
-const initialState = [];
-const store = createStore(initialState, reducer);
-console.log(store.dispatch(actions.addTodo(newTodoItem))); // Possible composition opportunity
+const initialState = undoableReducer();
+
+const store = createStore(initialState, undoableReducer);
+
+store.dispatch(actions.addTodo(newTodoItem)); // Possible composition opportunity
+store.dispatch(actions.completeTodo(id));
+store.dispatch(actions.unCompleteTodo(id));
+store.dispatch(actions.undo());
+store.dispatch(actions.redo());
+store.dispatch(actions.deleteTodo(id));
+console.log("state after delete", store.getState());
 
 export default () => {
   //   console.log(classes.main);
-
-  console.log(store.getState());
 };
